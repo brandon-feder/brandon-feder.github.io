4cb272cacc6c4f28bceb3127d03209751098e0b4
Brandon Feder <koala@brandonfeder.com>
2024-08-04 13:53:08 -0500

    added fluid background

--- assets/scripts/dropdown.js
+++ assets/scripts/dropdown.js
@@ -1,9 +1,9 @@
-$(".modal-open").on( "click", (eo) => {
-    proj = $(eo.target).siblings(".project-modal")
-    proj.addClass("active");
-})
+$(".modal-open").on("click", (eo) => {
+  proj = $(eo.target).siblings(".project-modal");
+  proj.addClass("active");
+});
 
-$(".modal-close").on( "click", (eo) => {
-    proj = $(eo.target).closest(".project-modal");
-    proj.removeClass("active");
-})
+$(".modal-close").on("click", (eo) => {
+  proj = $(eo.target).closest(".project-modal");
+  proj.removeClass("active");
+});
---
+++ assets/scripts/fluid.js
@@ -0,0 +1,6 @@
+let fluidCanvas = $("#fluidCanvas")[0];
+
+console.log(fluidCanvas);
+
+let myFluid = new Fluid(fluidCanvas);
+myFluid.activate();
